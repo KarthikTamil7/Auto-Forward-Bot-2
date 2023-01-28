83c3489f9548ff66db56c863460547ed08369023
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-01-28 08:45:53 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,4 +4,4 @@ class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5949999646:AAGNAzsUTMutsqtnSk2R4MkGgCQ0uMtqbIU")
     APP_ID = int(os.environ.get("APP_ID", 11973721))
     API_HASH = os.environ.get("API_HASH", "5264bf4663e9159565603522f58d3c18")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001342411240:-1001518309911"))
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001342411240"))
